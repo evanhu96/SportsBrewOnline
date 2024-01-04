2722e0c23153d6740e9d960eea89a630a27cdeed
evanhu96 <evanhughes66@yahoo.com>
2024-01-03 22:46:31 -0500

    Apollo

--- client/src/App.js
+++ client/src/App.js
@@ -29,6 +29,7 @@ const client = new ApolloClient({
 
 function App() {
   return (
+    <ApolloClient client={client}>
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
@@ -45,6 +46,7 @@ function App() {
           </a>
         </header>
       </div>
+    </ApolloClient>
   );
 }
 
