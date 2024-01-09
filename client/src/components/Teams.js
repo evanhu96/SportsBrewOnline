5da0acef6bf2d0d430a3e027acdffad5fcbd3e35
evanhu96 <evanhughes66@yahoo.com>
2024-01-09 18:32:55 -0500

    team images

--- client/src/components/Teams.js
+++ client/src/components/Teams.js
@@ -1,50 +1,76 @@
 import React, { useState } from "react";
-import { Button, ButtonGroup, Col, Stack } from "react-bootstrap";
+import { Button, ButtonGroup, Image, Stack } from "react-bootstrap";
 import "./app.css"; // Import a CSS file for custom styles
+import ATL from "../TeamPNGS/ATL.png";
+import BKN from "../TeamPNGS/BKN.png";
+import BOS from "../TeamPNGS/BOS.png";
+import CHA from "../TeamPNGS/CHA.png";
+import CHI from "../TeamPNGS/CHI.png";
+import CLE from "../TeamPNGS/CLE.png";
+import DAL from "../TeamPNGS/DAL.png";
+import DEN from "../TeamPNGS/DEN.png";
+import DET from "../TeamPNGS/DET.png";
+import GS from "../TeamPNGS/GS.png";
+import HOU from "../TeamPNGS/HOU.png";
+import IND from "../TeamPNGS/IND.png";
+import LAC from "../TeamPNGS/LAC.png";
+import LAL from "../TeamPNGS/LAL.png";
+import MEM from "../TeamPNGS/MEM.png";
+import MIA from "../TeamPNGS/MIA.png";
+import MIL from "../TeamPNGS/MIL.png";
+import MIN from "../TeamPNGS/MIN.png";
+import NO from "../TeamPNGS/NO.png";
+import NY from "../TeamPNGS/NY.png";
+import OKC from "../TeamPNGS/OKC.png";
+import ORL from "../TeamPNGS/ORL.png";
+import PHI from "../TeamPNGS/PHI.png";
+import PHX from "../TeamPNGS/PHX.png";
+import POR from "../TeamPNGS/POR.png";
+import SAS from "../TeamPNGS/SAS.png";
+import SAC from "../TeamPNGS/SAC.png";
+import TOR from "../TeamPNGS/TOR.png";
+import UTAH from "../TeamPNGS/UTAH.png";
+import WAS from "../TeamPNGS/WAS.png";
+const teamImages = {
+  ATL: ATL,
+  BKN: BKN,
+  BOS: BOS,
+  CHA: CHA,
+  CHI: CHI,
+  CLE: CLE,
+  DAL: DAL,
+  DEN: DEN,
+  DET: DET,
+  GS: GS,
+  HOU: HOU,
+  IND: IND,
+  LAC: LAC,
+  LAL: LAL,
+  MEM: MEM,
+  MIA: MIA,
+  MIL: MIL,
+  MIN: MIN,
+  NO: NO,
+  NYK: NY,
+  OKC: OKC,
+  ORL: ORL,
+  PHI: PHI,
+  PHX: PHX,
+  POR: POR,
+  SAS: SAS,
+  SAC: SAC,
+  TOR: TOR,
+  UTAH: UTAH,
+  WAS: WAS,
+};
 const NBATeams = ({ setTeam }) => {
   const teams = {
-    Atlantic: [
-      "Boston Celtics",
-      "Brooklyn Nets",
-      "New York Knicks",
-      "Philadelphia 76ers",
-      "Toronto Raptors",
-    ],
-    Central: [
-      "Chicago Bulls",
-      "Cleveland Cavaliers",
-      "Detroit Pistons",
-      "Indiana Pacers",
-      "Milwaukee Bucks",
-    ],
-    Southeast: [
-      "Atlanta Hawks",
-      "Charlotte Hornets",
-      "Miami Heat",
-      "Orlando Magic",
-      "Washington Wizards",
-    ],
-    Northwest: [
-      "Denver Nuggets",
-      "Minnesota Timberwolves",
-      "Oklahoma City Thunder",
-      "Portland Trail Blazers",
-      "Utah Jazz",
-    ],
-    Pacific: [
-      "Golden State Warriors",
-      "LA Clippers",
-      "Los Angeles Lakers",
-      "Phoenix Suns",
-      "Sacramento Kings",
-    ],
-    Southwest: [
-      "Dallas Mavericks",
-      "Houston Rockets",
-      "Memphis Grizzlies",
-      "New Orleans Pelicans",
-      "San Antonio Spurs",
-    ],
+    Atlantic: ["BOS", "BKN", "NYK", "PHI", "TOR"],
+    Central: ["CHI", "CLE", "DET", "IND", "MIL"],
+    Southeast: ["ATL", "CHA", "MIA", "ORL", "WAS"],
+    Northwest: ["DEN", "MIN", "OKC", "POR", "UTAH"],
+    Pacific: ["GS", "LAC", "LAL", "PHX", "SAC"],
+    Southwest: ["DAL", "HOU", "MEM", "NO", "SAS"],
   };
   const nbaTeams = {
     ATL: { abbreviation: "ATL", fullName: "Atlanta Hawks" },
@@ -79,31 +105,32 @@ const NBATeams = ({ setTeam }) => {
     WAS: { abbreviation: "WAS", fullName: "Washington Wizards" },
   };
 
-
   const changeTeam = (division, teamName) => {
     const teamAbb = Object.values(nbaTeams).find(
       (team) => team.fullName.toLowerCase() === teamName.toLowerCase()
     );
     setTeam(teamAbb.abbreviation);
-    console.log(division, teamName);
   };
-
   return (
     <div className="divisionContainer">
       {Object.entries(teams).map(([division, teamList]) => (
-          <Stack className="division">
-            <h2>{division}</h2>
-            <ButtonGroup className="TeamButtonGroup">
-              {teamList.map((team, index) => (
-                <Button
-                  className="teamButton"
-                  onClick={() => changeTeam(division, team)}
-                >
-                  {team}
-                </Button>
-              ))}
-            </ButtonGroup>
-          </Stack>
+        <Stack className="division">
+          <h2>{division}</h2>
+          <ButtonGroup className="TeamButtonGroup">
+            {teamList.map((team, index) => (
+              <Button
+                className="teamButton"
+                onClick={() => changeTeam(division, team)}
+              >
+                <img
+                  src={teamImages[team]}
+                  alt={team}
+                  style={{ height: "6vh" }}
+                />
+              </Button>
+            ))}
+          </ButtonGroup>
+        </Stack>
       ))}
     </div>
   );
